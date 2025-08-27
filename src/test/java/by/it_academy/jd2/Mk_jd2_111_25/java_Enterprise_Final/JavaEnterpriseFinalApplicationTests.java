6b3b13cd8c6639d2477c545d8df4ea2e98ccf059
Evgeny Prihodzko <evgenyprihodzko@gmail.com>
2025-08-27 21:03:10 +0300

    Initial commit

---
+++ src/test/java/by/it_academy/jd2/Mk_jd2_111_25/java_Enterprise_Final/JavaEnterpriseFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package by.it_academy.jd2.Mk_jd2_111_25.java_Enterprise_Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEnterpriseFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
