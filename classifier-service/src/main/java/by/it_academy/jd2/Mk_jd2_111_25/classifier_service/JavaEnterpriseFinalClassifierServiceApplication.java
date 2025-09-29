265b60a93c85097c93b6efe96cd01e3972ada378
Evgeny Prihodzko <evgenyprihodzko@gmail.com>
2025-09-29 14:48:34 +0300

    forgot annotation

--- classifier-service/src/main/java/by/it_academy/jd2/Mk_jd2_111_25/classifier_service/JavaEnterpriseFinalClassifierServiceApplication.java
+++ classifier-service/src/main/java/by/it_academy/jd2/Mk_jd2_111_25/classifier_service/JavaEnterpriseFinalClassifierServiceApplication.java
@@ -2,6 +2,7 @@ package by.it_academy.jd2.Mk_jd2_111_25.classifier_service;
 
 import by.it_academy.jd2.Mk_jd2_111_25.classifier_service.config.properties.JwtProperty;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableConfigurationProperties({
         JwtProperty.class
 })
+@SpringBootApplication
 public class JavaEnterpriseFinalClassifierServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaEnterpriseFinalClassifierServiceApplication.class, args);
